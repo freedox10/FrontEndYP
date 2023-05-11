347169af5cfff59902b2cb7865910e00ee803055
freedox10 <alfre2636@yahoo.com.ar>
2023-05-11 02:48:45 -0300

    Agregando Home y Login Components para routing

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
-export class NavBarComponent {
-  loginShow: boolean = false;
+export class NavBarComponent implements OnInit{
+  loginShow: boolean = true;
+
+  constructor(private router:Router) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  login(){
+    this.router.navigate(['/login'])
+  }
+
+  home(){
+    this.router.navigate(['/'])
+  }
 }
